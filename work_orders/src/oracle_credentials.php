d1ca22b3ba3a83a561a55a8b7d3477ee3bac4c68
Atreus <1581226+Atreus-X@users.noreply.github.com>
2026-01-26 17:12:32 -0500

    moved to api folder

--- work_orders/src/oracle_credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Store this file outside the web root if possible, or ensure .htaccess denies access
-$db_username = 'YOUR_USERNAME';
-$db_password = 'YOUR_PASSWORD';
-$db_connection_string = 'localhost/XE'; // e.g., //hostname:port/service_name
-?>
